18e55164a8377bf1d9b162a58eadf61d6bd25c48
Akshay Pall <iosakshay@gmail.com>
2016-03-31 00:50:49 -0400

    misc updates to song

--- Slife/Slife/app/src/main/java/com/example/akshaypall/slife/Song.java
+++ Slife/Slife/app/src/main/java/com/example/akshaypall/slife/Song.java
@@ -1,9 +1,15 @@
 package com.example.akshaypall.slife;
 
+import android.media.MediaMetadataRetriever;
+import android.net.Uri;
+import android.util.Log;
+
 /**
  * Created by Akshay Pall on 3/30/2016.
  */
 public class Song {
+    private static Uri MUSIC_URI= android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+
     private long mId;
     private String mName;
     private String mArtist;
@@ -30,5 +36,35 @@ public class Song {
 
     public String getmAlbum() {
         return mAlbum;
+    }
+
+    private String setTime () {
+        // load data file
+        MediaMetadataRetriever metaRetriever = new MediaMetadataRetriever();
+        metaRetriever.setDataSource(MUSIC_URI.toString());
+
+        String out = "";
+        // get mp3 info
+
+        // convert duration to minute:seconds
+        String duration =
+                metaRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
+        Log.v("time", duration);
+        long dur = Long.parseLong(duration);
+        String seconds = String.valueOf((dur % 60000) / 1000);
+
+        Log.v("seconds", seconds);
+        String minutes = String.valueOf(dur / 60000);
+        out = minutes + ":" + seconds;
+        if (seconds.length() == 1) {
+            txtTime.setText("0" + minutes + ":0" + seconds);
+        }else {
+            txtTime.setText("0" + minutes + ":" + seconds);
+        }
+        Log.v("minutes", minutes);
+        // close object
+        metaRetriever.release();
+
+        return minutes+":"+seconds;
     }
 }
